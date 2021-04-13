c2532f1e770a9e844d36ef596f04678f1dcce6be
luka <lka.v.lv@gmail.com>
2021-04-13 16:03:09 +0200

    startjs

--- script.js
+++ script.js
@@ -1,157 +1,123 @@
 const ListCountGame = class{
-    constructor(container, parent , child, total, footer, gameNumber){
-        this.$container = $(container);
-        this.$parent = $(parent);
-        this.$child =  $(child);
-        this.$total = $(total);
-        this.$footer = $(footer);
-        this.gameTotal = Math.floor(Math.random()*16)+15;
-        $(gameNumber).html(this.gameTotal );
-        this.addEvents();
-        this.createGameSequence();
+    constructor(child, parent, total, footer){
+      this.$child = $(child);
+      this.$parent = $(parent);
+      this.$total = $(total);
+      this.$footer = $(footer);
+      this.initEvents();
+      this.randomSequence();
+      this.calcTotal();
+    }
+  
+    initEvents(){
+      this.$child.on('click',(e)=>{
+        this.appendItem(e);
         this.calcTotal();
+      })
     }
-
-    addEvents(){
-        
-      this.$child.on('click', this.$child ,(e)=>{
-            this.appendTarget(e);
-            // this.calcTotal();
-            // this.checkWon();
-        });
+  
+    appendItem(e){
+      const $clickedItem = $(e.currentTarget);
+      const $clickedParent = $clickedItem.parent();
+      const clickedLeftList = $clickedParent.is(this.$parent.first());
+      
+      // append item to the other list
+      if (clickedLeftList) {
+        this.$parent.last().append($clickedItem);
+      } else {
+        this.$parent.first().append($clickedItem);
+      }
     }
-
-    appendTarget(e){
-        this.currentContainer = $(e.currentTarget).closest(this.$container);
-        let targetParent;
-        if (this.currentContainer.next().length) {
-            targetParent = this.currentContainer.next().find(this.$parent);
-            
-        }else{
-            targetParent = this.currentContainer.prev().find(this.$parent);
-        }
-
-        $(e.currentTarget).addClass("removeCard");
-
-        var cssTransitionEnd = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
-        $(e.currentTarget).on(cssTransitionEnd, (event)=>{
-            if(event.originalEvent.propertyName  !== 'margin-top') return;
-            setTimeout(()=>{
-                $(e.currentTarget).removeClass("removeCard").appendTo(targetParent);
-                this.calcTotal();
-                this.checkWon();
-            }, 100)
-        })
-    }
-
+  
     calcTotal(){
-        this.$container.each((i,element)=>{
-            const $listItems = $(element).find("li")
-            const total = this.sum($listItems)
-            $(element).find(this.$total).html(total);
-        })
-    }
-
-    generateSequence(){
-        this.gameTotal
-        const cardAmount = Math.floor(Math.random()*3)+2 //2-5
-        const sequence = [];
-        let total = this.gameTotal;
-        for (let steps = cardAmount; steps >0; steps--) {
-            // 20
-            // 0-19
-            // 1-20 ==> 1-17
-            // 3steps
-
-            // 1-17 
-            // 2steps (total 3)
-            // 3 -2 ==> 1
-            // 1step (total 2)
-            // 
-            // 1==> 1
-            // 1
-            
-            let maxRandomNumber = total-steps;
-            const sequenceNumber = Math.floor(Math.random()*maxRandomNumber)+1
-            sequence.push(sequenceNumber);
-            total -= sequenceNumber;
-        }
-        sequence.push(total);
-        console.log(sequence);
-        console.log(sequence.reduce((a, b) => a + b, 0) );
-        return sequence;
-    }
-
-    createGameSequence(){
-        let gameArray = this.generateSequence().concat(this.generateSequence());
-        gameArray = this.shuffleArray(gameArray);
-        const array1 = gameArray.slice(0,gameArray.length/2);
-        const array2 = gameArray.slice(gameArray.length/2, gameArray.length);
-        this.makeListItems(this.$parent.first(),array1);
-        this.makeListItems(this.$parent.last(),array2);
-        console.log($(this.$child, this.$parent));
-    }
-
-    shuffleArray(array){
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    makeListItems($domContainer,sequence){
-        sequence.forEach(number => {
-            $("<li></li>").text(number).appendTo($domContainer);
+      this.$parent.each((index, parent)=>{
+        const $children = $(parent).find(this.$child);
+        let total = 0;
+        // sum of list items
+        $children.text((index, text)=>{
+          if (isNaN(parseInt(text))) return;
+          total += parseInt(text);
         });
+
+        $(parent).next().find(this.$total).text(total);
+      })
     }
 
-    sum($doms){
-        let result = 0;
-        $doms.text( (index, el) => {
-            result += parseInt(el);
-        });
-        return result;
-    }
-
-
-    checkWon(){
-        const total = parseInt(this.$container.last().find(this.$total).html());
-        if (total === this.gameTotal) {
-            this.$footer.addClass("locked");
-            this.$parent.off('click');
+    randomSequence(){
+        this.gameTotal = 20;
+        const stepAmount = this.random(3,4); //4
+        let prevAmount = 0;
+        let sequence = [];
+        for (let stepsLeft = stepAmount-1; stepsLeft >0; stepsLeft--) {
+            const maxNumber = this.gameTotal-prevAmount-stepsLeft;
+            const randomNumber = this.random(1,maxNumber);
+            prevAmount += randomNumber;
+            sequence.push(randomNumber);
         }
+        const restValue = this.gameTotal - prevAmount;
+        sequence.push(restValue);
+        console.log('totalSequence:', sequence.reduce((a,b) => a + b, 0));
+        console.log('sequence:', sequence);
     }
 
-}
 
-new ListCountGame(".js-container",".js-list", "li", ".js-total", ".js-footer", ".js-gameNumber")
+    random(min, max){
+      return  Math.floor(Math.random()*(max+1-min))+min;
+    }
 
-// 20 => [2, 4, 2, ]
+    checkWon(total){
+      if (total == 0) {
+        this.$footer.addClass("locked");
+        this.$child.off('click');
+      }
+    }
 
-// 20 => [5, 4, 4,2, 5]
+  }
+  
+  new ListCountGame('li','.js-list', ".js-total", ".js-footer");
+
+// Simplified version of 5 steps:
+/* #1  Recources? start?   ####
+   - startcijfer = getal tussen 15-30
+   - 6-8 random getallen 
+*/
+/* #2 Objectives? during?  ####
+  - move items  between lists
+  - recalculate total 
+  - find the correct sequence
+*/
+/* #3 Obstacles? constant? ####
+  - given total number (20)
+  - randomized sequence
+*/
+/* #4    Interaction?      ####
+  - click list item
+*/
+/* #5        Goal?         ####
+   - 2 kollomen met eenzelfde totaal
+   - het totaal wordt gevormd met random getllane
+*/
 
 
-// generating
-// [8, 5, 6, 7, 9,5] ==> 2x = 10
-// generate random numbers ==> check if there is a possible solution
-// if not==> regenerate
+/*------------------------
+  |--- Game-generator ---|
+  ------------------------
+*/
+// StartNumber: 20
+// [17,1,1,1]
 
-// [3,4,6] <=> 13   keep generating next one with same sum [10,3] => 13
-// [1,1,13] <=> 15
+//##step1## (10)
+// 20-0-3    = 17-1 ==> 10
+//##step2## (5) 
+// 20-10-2 = 8-1  ==> 5
+//##step3## (3)
+// 20-15-1 = 4-1  ==> 3
+//##step4## (1)
+// 20-(10+5+3) = 2
 
-// [1,1,1,12] <=> 15
-// [1,1,1,1,11] <=> 15
-
-// Logica ==> what does always stay the same?
-// 20
-// ==> 20-10-5-5 =0
-// 18-1 = 10
-// 20-10 = 10
-// 9-1 = 5
-// 10-5 = 5
-// [10,5,5]
+// =============> sum() = 20
 
 
-// what is er constant? what does the gave give? (resources)
-// 20
+// 2x lijsten  [17,1,1,1] =20 & [5,5,4,4,2]= 20
+
+// [17,1,4,4,2] & [1,1,5,5]
